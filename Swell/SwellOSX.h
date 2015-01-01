a11d310902fc670c94e72781d1e81537e36c4be0
David Mann <ninja@codingninja.com.au>
2015-01-02 10:36:43 +1100

    Add OSX Build scheme

---
+++ Swell/SwellOSX.h
@@ -0,0 +1,17 @@
+//
+//  Swell.h
+//  Swell
+//
+//  Created by Hubert Rabago on 6/20/14.
+//  Copyright (c) 2014 Minute Apps LLC. All rights reserved.
+//
+
+//! Project version number for Swell.
+FOUNDATION_EXPORT double SwellVersionNumber;
+
+//! Project version string for Swell.
+FOUNDATION_EXPORT const unsigned char SwellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swell/PublicHeader.h>
+
+//#import "Swell/Swell-Swift.h"
